d588948194d6c29a59802ff26de3dbae24689a5c
pars5555 <pars5555@yahoo.com>
2015-03-20 18:53:43 +0400

    111

--- htdocs/g9f2788cer8f9a608d759df25d251b69.php
+++ htdocs/g9f2788cer8f9a608d759df25d251b69.php
@@ -1,5 +1,5 @@
 <?php
-exec('sh /var/www/rpijava_git_update.sh  2>&1', $output);
+exec('sh /var/www/scripts/rpijava_git_update.sh  2>&1', $output);
 if (is_array($output))
 {
     foreach($output as $out)
